c276ce7a84d7b3741a66328bdcc7074b1d8b107f
DAVID <davidsansonii@hotmail.com>
2020-07-19 23:27:22 -0500

    Config Var

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 //Base de datos
-let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://mdbJonathan:mdb2020@JMG@cluster0.xyioe.mongodb.net/cafe';
+let urlDB = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/cafe' : process.env.MONGO_URI;
 process.env.URLDB = urlDB;
